7fa2628cb0b4e37670cee5d487a2e7843e418534
martinjakal <49129874+martinjakal@users.noreply.github.com>
2021-08-23 20:28:10 +0200

    Create euler009.cpp

---
+++ solution/euler009.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Project Euler - Problem 9
+// https://projecteuler.net/problem=9
+// Special Pythagorean triplet
+// Result: 31875000
+
+int findPythagoreanTripletProduct(int limit)
+{
+    for (int a = 1; a < limit / 3; ++a) // a < b < c
+    {
+        for (int b = a + 1; b < limit - a - b; ++b)
+        {
+            int c = limit - a - b;
+
+            if (a * a + b * b == c * c)
+                return a * b * c;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int limit = 1000;
+    auto result = findPythagoreanTripletProduct(limit);
+    std::cout << result << std::endl;
+
+    return 0;
+}
