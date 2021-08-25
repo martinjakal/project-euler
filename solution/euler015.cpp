303e71200db878b444725f6ecd8c26df25df3e82
martinjakal <49129874+martinjakal@users.noreply.github.com>
2021-08-25 21:25:31 +0200

    Create euler015.cpp

---
+++ solution/euler015.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// Project Euler - Problem 15
+// https://projecteuler.net/problem=15
+// Lattice paths
+// Result: 137846528820
+
+// Both directions occur n times in square grid with size n, that is (2n n) combinations.
+unsigned long long latticePathsInSquareGrid(int gridSize)
+{
+    unsigned long long paths = 1;
+
+    for (unsigned long long i = 1; i <= gridSize; ++i)
+    {
+        paths *= i + gridSize;
+        paths /= i;
+    }
+
+    return paths;
+}
+
+int main()
+{
+    int gridSize = 20;
+    auto result = latticePathsInSquareGrid(gridSize);
+    std::cout << result << std::endl;
+
+    return 0;
+}
