549d3231e3b6d0077d48a0acb7c2982b4116fd56
martinjakal <49129874+martinjakal@users.noreply.github.com>
2021-09-02 13:58:21 +0200

    Create euler040.cpp

---
+++ solution/euler040.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Project Euler - Problem 40
+// https://projecteuler.net/problem=40
+// Champernowne's constant
+// Result: 210 
+
+int productOfDigitsChampernowne(const std::vector<int>& digits)
+{
+    const int maxDigit = *std::max_element(digits.begin(), digits.end());
+    std::string champernowne;
+    int product = 1;
+
+    for (int i = 1; static_cast<int>(champernowne.size()) < maxDigit; ++i)
+        champernowne.append(std::to_string(i));
+
+    for (auto digit : digits)
+        product *= std::stoi(champernowne.substr(digit - 1, 1));
+
+    return product;
+}
+
+int main()
+{
+    std::vector<int> digits = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
+    auto result = productOfDigitsChampernowne(digits);
+    std::cout << result << std::endl;
+
+    return 0;
+}
