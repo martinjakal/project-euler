103d802097d41d209f0820fb7984413c683562d9
martinjakal <49129874+martinjakal@users.noreply.github.com>
2021-10-15 19:56:34 +0200

    Create euler073.cpp

---
+++ solution/euler073.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <numeric>
+
+// Project Euler - Problem 73
+// https://projecteuler.net/problem=73
+// Counting fractions in a range
+// Result: 7295372 
+
+int countFractionsInRange(int limit)
+{
+    int fractionCnt = 0;
+
+    for (int d = 1; d <= limit; ++d)
+    {
+        for (int n = d / 3 + 1; n <= (d - 1) / 2; ++n)
+        {
+            if (std::gcd(n, d) == 1)
+                ++fractionCnt;
+        }
+    }
+
+    return fractionCnt;
+}
+
+int main()
+{
+    int limit = 12000;
+    auto result = countFractionsInRange(limit);
+    std::cout << result << std::endl;
+
+    return 0;
+}
