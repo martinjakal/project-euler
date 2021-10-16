63dcb2b374a2aa957d0671df2e9e4d7d0d94a1ec
martinjakal <49129874+martinjakal@users.noreply.github.com>
2021-10-16 22:01:14 +0200

    Create euler055.cpp

---
+++ solution/euler055.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include <helper/BigInteger.hpp>
+
+// Project Euler - Problem 55
+// https://projecteuler.net/problem=55
+// Lychrel numbers
+// Result: 249
+
+int countLychrelNumbers(int limit, int maxIter)
+{
+    int lychrelCnt = limit - 1;
+
+    for (int i = 1; i < limit; ++i)
+    {
+        BigInteger number(i);
+
+        for (int j = 1; j <= maxIter; ++j)
+        {
+            number += number.reverse();
+
+            if (number.isPalindrome())
+            {
+                --lychrelCnt;
+                break;
+            }
+        }
+    }
+
+    return lychrelCnt;
+}
+
+int main()
+{
+    int limit = 10000;
+    int maxIteration = 50;
+    auto result = countLychrelNumbers(limit, maxIteration);
+    std::cout << result << std::endl;
+
+    return 0;
+}
