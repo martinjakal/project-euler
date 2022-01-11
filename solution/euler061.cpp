154e6b0473cad9bb05f379baf7bef79ae410650e
martinjakal <martinjakal93@gmail.com>
2022-01-11 17:14:43 +0100

    Update euler061.cpp

--- solution/euler061.cpp
+++ solution/euler061.cpp
@@ -8,33 +8,8 @@
 // Cyclical figurate numbers
 // Result: 28684
 
-bool findNextLink(const std::vector<std::vector<int>>& figurate, int idx, std::vector<int>& chain)
+auto generateFigurateNumbersInRange(int minRange, int maxRange) -> std::vector<std::vector<int>>
 {
-    for (auto number : figurate[idx])
-    {
-        if (chain.back() % 100 != number / 100 || std::find(chain.begin(), chain.end(), number) != chain.end())
-            continue;
-
-        chain.push_back(number);
-
-        // If the chain contains distinct number from each set, check the final loop. 
-        if (chain.size() == figurate.size() && number % 100 == chain[0] / 100)
-            return true;
-
-        // Advance to the next set of numbers if there is any.
-        if (chain.size() < figurate.size() && findNextLink(figurate, idx + 1, chain))
-            return true;
-
-        chain.pop_back();
-    }
-
-    return false;
-}
-
-int sumCyclicalFigurateNumbers()
-{
-    const int minRange = 1000;
-    const int maxRange = 10000;
     std::vector<std::vector<int>> figurateNumbers(6);
 
     for (int i = 1, n = 0; n < maxRange; ++i, n = i * (i + 1) / 2) // triangle
@@ -73,9 +48,41 @@ int sumCyclicalFigurateNumbers()
             figurateNumbers[5].push_back(n);
     }
 
+    return figurateNumbers;
+}
+
+bool findNextLink(const std::vector<std::vector<int>>& figurate, int idx, std::vector<int>& chain)
+{
+    for (auto number : figurate[idx])
+    {
+        if (chain.back() % 100 != number / 100 || std::find(chain.begin(), chain.end(), number) != chain.end())
+            continue;
+
+        chain.push_back(number);
+
+        // If the chain contains distinct number from each set, check the final loop. 
+        if (chain.size() == figurate.size() && number % 100 == chain.front() / 100)
+            return true;
+
+        // Advance to the next set of numbers if there is any.
+        if (chain.size() < figurate.size() && findNextLink(figurate, idx + 1, chain))
+            return true;
+
+        chain.pop_back();
+    }
+
+    return false;
+}
+
+int sumCyclicalFigurateNumbers()
+{
+    const int minRange = 1000;
+    const int maxRange = 10000;
+    auto figurateNumbers = generateFigurateNumbersInRange(minRange, maxRange);
+
     std::sort(figurateNumbers.begin(), figurateNumbers.end());
 
-    // Different permutations of number sets are checked. Starting from the first set, use backtracking to find 
+    // Different permutations of number sets are checked. Starting from the first set, backtracking is used to find 
     // the chain of distinct numbers from the following sets connected by digits.
     do
     {
