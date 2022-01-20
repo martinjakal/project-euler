6136548c4a2858143d8e5210a6bb47473cef1a15
martinjakal <martinjakal93@gmail.com>
2022-01-20 20:49:17 +0100

    Create euler070.cpp

---
+++ solution/euler070.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+#include <helper/MathPackage.hpp>
+
+// Project Euler - Problem 70
+// https://projecteuler.net/problem=70
+// Totient permutation
+// Result: 8319823
+
+using namespace math;
+
+// Euler's totient function is used: phi(n) = n * product (1 - 1 / p), where p is a prime which divides n.
+// To minimize n / phi(n), the number of primes in the product must be minimized. At least two primes must be used,
+// because if n is a prime, then phi(n) = n - 1 and n - 1 is never a permutation of n.
+// Also phi(m * n) = phi(m) * phi(n) * d / phi(d), where d = gcd(m, n).
+// If m and n are prime, then phi(m * n) = phi(m) * phi(n).
+int minTotientPermutation(int limit)
+{
+    auto primes = sieveOfEratosthenes(limit / 2);
+    double minRatio = std::numeric_limits<double>::max();
+    int numberWithMinRatio = 0;
+
+    for (int i = 0; i < primes.size(); ++i)
+    {
+        for (int j = i; j < primes.size() && 1ull * primes[i] * primes[j] < limit; ++j)
+        {
+            int number = primes[i] * primes[j];
+            int totient = (primes[i] - 1) * (primes[j] - 1);
+
+            if (!isPermutation(number, totient))
+                continue;
+
+            double ratio = static_cast<double>(number) / totient;
+
+            if (ratio < minRatio)
+            {
+                minRatio = ratio;
+                numberWithMinRatio = number;
+            }
+        }
+    }
+
+    return numberWithMinRatio;
+}
+
+int main()
+{
+    int limit = 10000000;
+    auto result = minTotientPermutation(limit);
+    std::cout << result << std::endl;
+
+    return 0;
+}
