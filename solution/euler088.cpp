a4fe5104fbdd6ee22bbe0608c0adfcaea5ce2679
martinjakal <martinjakal93@gmail.com>
2022-03-11 13:12:18 +0100

    Update euler088.cpp

--- solution/euler088.cpp
+++ solution/euler088.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <numeric>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 // Project Euler - Problem 88
@@ -9,7 +9,7 @@
 // Product-sum numbers
 // Result: 7587457
 
-void findProductSumNumbers(std::vector<int>& data, int start, int depth = 0, int sum = 0, int prod = 1)
+void findProductSumNumbers(std::vector<int>& data, int start = 2, int depth = 0, int sum = 0, int prod = 1)
 {
     int count = depth + prod - sum;
     data[count] = std::min(prod, data[count]);
@@ -30,7 +30,7 @@ auto findProductSumNumbers(int limit) -> std::vector<int>
 {
     std::vector<int> productSums(limit + 1, std::numeric_limits<int>::max());
     productSums[1] = 1;
-    findProductSumNumbers(productSums, 2);
+    findProductSumNumbers(productSums);
     return productSums;
 }
 
@@ -38,7 +38,7 @@ int sumProductSumNumbers(int limit)
 {
     const auto productSumNumbers = findProductSumNumbers(limit);
 
-    std::set<int> sums;
+    std::unordered_set<int> sums;
     for (int i = 2; i <= limit; i++)
         sums.insert(productSumNumbers[i]);
 
