ad78381e4af7d248c226df151668d226b3ea1ca0
martinjakal <martinjakal93@gmail.com>
2022-04-06 21:37:53 +0200

    Updated e17

--- solution/euler017.cpp
+++ solution/euler017.cpp
@@ -9,12 +9,12 @@
 int countLettersInNumbers(int limit)
 {
     const std::unordered_map<int, int> numberLetters = { { 1, 3 }, { 2, 3 }, { 3, 5 }, { 4, 4 }, { 5, 4 }, { 6, 3 },
-        { 7, 5 }, { 8, 5 }, { 9, 4 }, { 10, 3 }, { 11, 6 }, { 12, 6 }, { 13, 8 }, { 14, 8 }, { 15, 7 }, { 16, 7 }, 
-        { 17, 9 }, { 18, 8 }, { 19, 8 }, { 20, 6 }, { 30, 6 }, { 40, 5 }, { 50, 5 }, { 60, 5 }, { 70, 7 }, { 80, 6 }, 
+        { 7, 5 }, { 8, 5 }, { 9, 4 }, { 10, 3 }, { 11, 6 }, { 12, 6 }, { 13, 8 }, { 14, 8 }, { 15, 7 }, { 16, 7 },
+        { 17, 9 }, { 18, 8 }, { 19, 8 }, { 20, 6 }, { 30, 6 }, { 40, 5 }, { 50, 5 }, { 60, 5 }, { 70, 7 }, { 80, 6 },
         { 90, 6 }, { 100, 7 }, { 1000, 8 } };
-    
+
     int letterCnt = 0;
-    
+
     for (int i = 1; i <= limit; ++i)
     {
         int thousands = i / 1000 % 10;
@@ -27,7 +27,7 @@ int countLettersInNumbers(int limit)
         if (hundreds > 0)
             letterCnt += numberLetters.find(hundreds)->second + numberLetters.find(100)->second;
 
-        if ((thousands > 0 || hundreds > 0) && belowHundred > 0) // for "and" word
+        if ((thousands > 0 || hundreds > 0) && belowHundred > 0) // add "and" word
             letterCnt += 3;
 
         if (belowHundred > 0)
@@ -35,7 +35,7 @@ int countLettersInNumbers(int limit)
             if (belowHundred < 20 || belowHundred % 10 == 0)
                 letterCnt += numberLetters.find(belowHundred)->second;
             else
-                letterCnt += numberLetters.find(belowHundred / 10 * 10)->second + numberLetters.find(belowHundred % 10)->second;
+                letterCnt += numberLetters.find(belowHundred - belowHundred % 10)->second + numberLetters.find(belowHundred % 10)->second;
         }
     }
 
