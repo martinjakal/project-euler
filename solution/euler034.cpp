bbe60c3982c4962d4e7284afd21435164c10c46c
martinjakal <martinjakal93@gmail.com>
2022-04-11 20:36:33 +0200

    Updated e34

--- solution/euler034.cpp
+++ solution/euler034.cpp
@@ -10,42 +10,43 @@
 
 using namespace math;
 
-int digitFactorial(int number, const std::vector<int>& factorial)
+int sumDigitFactorials(int number, const std::vector<int>& factorials)
 {
     int sum = 0;
 
     while (number > 9)
     {
-        sum += factorial[number % 10];
+        sum += factorials[number % 10];
         number /= 10;
     }
 
-    return sum + factorial[number];
+    return sum + factorials[number];
 }
 
 // Adding extra digit (assume it is a 9) is equal to: lhs * 10 + 9, rhs + 9!.
 // Lhs grows faster and becames greater than rhs at 7 digits.
-// The upper bound for the number becomes 7 * 9! = 2540160.
-int sumDigitFactorials()
+// Therefore the upper bound for the number is 7 * 9! = 2540160.
+int sumFactorions()
 {
     const int limit = 2540160;
-    int sum = 0;
-    std::vector<int> factorialStore;
+    int sumFact = 0;
+
+    std::vector<int> factorialsCache;
     for (int i = 0; i < 10; ++i)
-        factorialStore.push_back(factorial(i));
+        factorialsCache.push_back(factorial(i));
 
     for (int i = 3; i < limit; ++i)
     {
-        if (digitFactorial(i, factorialStore) == i)
-            sum += i;
+        if (sumDigitFactorials(i, factorialsCache) == i)
+            sumFact += i;
     }
 
-    return sum;
+    return sumFact;
 }
 
 int main()
 {
-    auto result = sumDigitFactorials();
+    auto result = sumFactorions();
     std::cout << result << std::endl;
 
     return 0;
