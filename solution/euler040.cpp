9ead6525b04d11731f165654d744fe3375a5332d
martinjakal <martinjakal93@gmail.com>
2022-04-11 21:17:23 +0200

    Updated e40

--- solution/euler040.cpp
+++ solution/euler040.cpp
@@ -8,25 +8,26 @@
 // Champernowne's constant
 // Result: 210 
 
-int productOfDigitsChampernowne(const std::vector<int>& digits)
+int calcProductOfDigitsChampernowne(const std::vector<int>& digits)
 {
-    const size_t maxDigit = *std::max_element(digits.begin(), digits.end());
+    std::size_t maxDigit = *std::max_element(digits.begin(), digits.end());
     std::string champernowne;
     int product = 1;
 
     for (int i = 1; champernowne.size() < maxDigit; ++i)
-        champernowne.append(std::to_string(i));
+        champernowne += std::to_string(i);
 
     for (auto digit : digits)
-        product *= std::stoi(champernowne.substr(digit - 1, 1));
+        product *= champernowne[digit - 1] - '0';
 
     return product;
 }
 
 int main()
 {
-    std::vector<int> digits = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
-    auto result = productOfDigitsChampernowne(digits);
+    std::vector<int> digits = { 1, 10, 100, 1000, 10000, 100'000, 1'000'000 };
+
+    auto result = calcProductOfDigitsChampernowne(digits);
     std::cout << result << std::endl;
 
     return 0;
