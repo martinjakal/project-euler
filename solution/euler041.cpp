041336b50781ad1495d22360d533fbf5ec01dfcc
martinjakal <martinjakal93@gmail.com>
2022-04-12 18:58:48 +0200

    Updated e41

--- solution/euler041.cpp
+++ solution/euler041.cpp
@@ -11,33 +11,34 @@
 
 using namespace math;
 
-// Pandigital prime can only have max 7 digits, because digit sums for 8 (36) and 9 (45) 
-// digits are divisible by 3, meaning the number is also divisible by 3. 
-int maxPandigitalPrime()
+// Pandigital primes can only have max 7 digits, because digit sums for 8 (36) and 9 (45)
+// digits are divisible by 3, meaning the number is also divisible by 3.
+int findMaxPandigitalPrime()
 {
     std::string digits;
-    int maxPrime = 0;
-    
+    int maxPandigitalPrime = 0;
+
     for (int i = 1; i < 8; ++i)
     {
-        digits.append(std::to_string(i));
+        digits += std::to_string(i);
 
         do
         {
             int number = std::stoi(digits);
 
-            if (isPrime(number))
-                maxPrime = std::max(number, maxPrime);
-
+            if (isPrime(number) && number > maxPandigitalPrime)
+            {
+                maxPandigitalPrime = number;
+            }
         } while (std::next_permutation(digits.begin(), digits.end()));
-    } 
+    }
 
-    return maxPrime;
+    return maxPandigitalPrime;
 }
 
-int main() 
+int main()
 {
-    auto result = maxPandigitalPrime();
+    auto result = findMaxPandigitalPrime();
     std::cout << result << std::endl;
 
     return 0;
