72f920eb37ac09db4e22d5e8b97a6f2646f06080
martinjakal <martinjakal93@gmail.com>
2022-04-13 21:20:30 +0200

    Updated e48

--- solution/euler048.cpp
+++ solution/euler048.cpp
@@ -13,11 +13,7 @@ auto sumSelfPowers(int maxPower, int lastDigits) -> std::string
     BigInteger sumPowers;
 
     for (int i = 1; i <= maxPower; ++i)
-    {
-        BigInteger number(i);
-        number ^= i;
-        sumPowers += number;
-    }
+        sumPowers += BigInteger(i) ^ BigInteger(i);
 
     return sumPowers.print().substr(sumPowers.digitCnt() - lastDigits);
 }
@@ -26,6 +22,7 @@ int main()
 {
     int maxPower = 1000;
     int lastDigits = 10;
+
     auto result = sumSelfPowers(maxPower, lastDigits);
     std::cout << result << std::endl;
 
