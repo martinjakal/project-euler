95760c706bc66bfad37a6f0c22c7d437783a000d
martinjakal <martinjakal93@gmail.com>
2022-04-16 10:16:29 +0200

    Updated e63

--- solution/euler063.cpp
+++ solution/euler063.cpp
@@ -5,27 +5,27 @@
 // Powerful digit counts
 // Result: 49
 
-// Following trick is used: if a ^ b = c, then (a ^ b) / (10 ^ b) = c / (10 ^ b) and c / (10 ^ b) < 1. 
-// For example, 9 ^ 3 = 729 and 0.9 ^ 3 = (9 ^ 3) / (10 ^ 3) = 0.729. The result gets lower after 
-// each multiplication by a / 10. When the result gets below 0.1, it means the denominator (power of 10) 
+// The following trick is used: if a ^ b = c, then (a / 10) ^ b = c / (10 ^ b) and rhs < 1.
+// For example, 9 ^ 3 = 729 and 0.9 ^ 3 = 0.729. The result gets lower after each multiplication
+// by a / 10. When the result gets below 0.1, it means the denominator (power of 10)
 // has more digits then numerator (power of a).
 int countNthPowerNumbers()
 {
-    int nthPowerCount = 0;
+    int nthPowerCnt = 0;
 
     for (int i = 1; i < 10; ++i)
     {
-        const double change = 0.1 * i;
-        double number = change;
+        const double diff = 0.1 * i;
+        double number = diff;
 
         while (number >= 0.1)
         {
-            ++nthPowerCount;
-            number *= change;
+            ++nthPowerCnt;
+            number *= diff;
         }
     }
 
-    return nthPowerCount;
+    return nthPowerCnt;
 }
 
 int main()
