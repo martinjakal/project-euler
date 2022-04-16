380cf5e0b3e57a07ee495551149bd3c5d117a35d
martinjakal <martinjakal93@gmail.com>
2022-04-16 14:27:11 +0200

    Updated e69

--- solution/euler069.cpp
+++ solution/euler069.cpp
@@ -11,23 +11,24 @@ using namespace math;
 
 // Euler's totient function is used: phi(n) = n * product (1 - 1 / p), where p is a prime which divides n.
 // To maximize n / phi(n), the product of primes must be maximized.
-int maxTotient(int limit)
+int findNumberWithMaxTotient(int limit)
 {
     int totient = 1;
 
-    for (int number = 2; totient * number < limit; ++number)
+    for (int number = 2; totient * number <= limit; ++number)
     {
         if (isPrime(number))
             totient *= number;
-    }   
+    }
 
     return totient;
 }
 
 int main()
 {
-    int limit = 1000000;
-    auto result = maxTotient(limit);
+    int limit = 1'000'000;
+
+    auto result = findNumberWithMaxTotient(limit);
     std::cout << result << std::endl;
 
     return 0;
