c31855aadb4e86f2d2ff7225c15260392263d9bd
martinjakal <martinjakal93@gmail.com>
2022-04-18 19:18:28 +0200

    Ref

--- solution/euler025.cpp
+++ solution/euler025.cpp
@@ -9,10 +9,10 @@
 
 int findFibonacciTermWithDigits(int digits)
 {
-    int termIdx = 2;
     BigInteger f0 = 0;
     BigInteger f1 = 1;
     BigInteger f2 = 1;
+    int termIdx = 2;
 
     while (f2.digitCnt() != digits)
     {
