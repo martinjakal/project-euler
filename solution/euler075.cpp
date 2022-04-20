4aa006cd53fd99bd562e7dc1ec8d4947d8920d47
martinjakal <martinjakal93@gmail.com>
2022-04-20 21:32:39 +0200

    Updated e75

--- solution/euler075.cpp
+++ solution/euler075.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <numeric>
 #include <unordered_map>
@@ -8,17 +9,19 @@
 // Singular integer right triangles
 // Result: 161667
 
-int singleSolutionRightTriangles(int limit)
+int countPerimetersWithSingleRightTriangle(int limit)
 {
-    std::unordered_map<int, int> solutions;
-
     // Euclid's formula is used to generate Pythagorean triples.
     // For n = 0, the formula becomes 2 * m^2 <= limit.
-    for (int m = 2; m <= sqrt(limit / 2); ++m)
+    const int maxCoeff = static_cast<int>(std::sqrt(limit / 2));
+
+    std::unordered_map<int, int> solutions;
+
+    for (int m = 2; m <= maxCoeff; ++m)
     {
         for (int n = 1; n < m; ++n)
         {
-            // Numbers must be coprime and not both odd. 
+            // Coefficients must be coprime and not both odd. 
             if (std::gcd(m, n) != 1 || (m % 2 == 1 && n % 2 == 1))
                 continue;
 
@@ -30,14 +33,16 @@ int singleSolutionRightTriangles(int limit)
         }
     }
 
-    return std::count_if(solutions.begin(), solutions.end(), [](const std::pair<int, int>& s) { return s.second == 1; });
+    return static_cast<int>(std::count_if(solutions.begin(), solutions.end(),
+        [](const std::pair<int, int>& s) { return s.second == 1; }));
 }
 
 int main()
 {
-    int limit = 1500000;
-    auto result = singleSolutionRightTriangles(limit);
-    std::cout << result << std::endl; 
+    int limit = 1'500'000;
+
+    auto result = countPerimetersWithSingleRightTriangle(limit);
+    std::cout << result << std::endl;
 
     return 0;
 }
