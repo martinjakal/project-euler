9eb1dd7f1d340df34a1363c0d90dc0019f098575
martinjakal <martinjakal93@gmail.com>
2022-04-20 19:48:57 +0200

    Refactoring

--- solution/euler043.cpp
+++ solution/euler043.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 // Project Euler - Problem 43
@@ -7,37 +8,34 @@
 // Sub-string divisibility
 // Result: 16695334890
 
+const std::vector<int> primes = { 2, 3, 5, 7, 11, 13, 17 };
+const int primeCnt = static_cast<int>(primes.size());
+
+// Check if the digit substrings are divisible by respective primes.
+bool hasDivisibleSubstrings(const std::vector<int>& digits)
+{
+    for (int i = 0; i < primeCnt; ++i)
+    {
+        int substr = digits[i + 1] * 100 + digits[i + 2] * 10 + digits[i + 3];
+
+        if (substr % primes[i] != 0)
+            return false;
+    }
+
+    return true;
+}
+
 unsigned long long sumDivisiblePandigitalNumbers()
 {
-    const std::vector<int> primes = { 2, 3, 5, 7, 11, 13, 17 };
-    const int primeCnt = static_cast<int>(primes.size());
     std::vector<int> digits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     unsigned long long sumDivisiblePandigital = 0;
 
     do
     {
-        bool divisible = true;
-
-        // Check if the substrings are divisible by respective primes.
-        for (int i = 0; i < primeCnt; ++i)
+        if (hasDivisibleSubstrings(digits))
         {
-            int substr = digits[i + 1] * 100 + digits[i + 2] * 10 + digits[i + 3];
-
-            if (substr % primes[i] != 0)
-            {
-                divisible = false;
-                break;
-            }
-        }
-
-        if (divisible)
-        {
-            unsigned long long number = 0;
-
-            for (auto d : digits)
-                number = number * 10 + d;
-
-            sumDivisiblePandigital += number;
+            sumDivisiblePandigital += std::accumulate(digits.begin(), digits.end(), 0ull,
+                [](unsigned long long number, int d) { return number * 10 + d; });
         }
     } while (std::next_permutation(digits.begin(), digits.end()));
 
