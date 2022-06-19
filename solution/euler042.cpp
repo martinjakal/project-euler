18fa095400cff4428770422c7f0a9f795aa5a957
martinjakal <martinjakal93@gmail.com>
2022-06-19 15:23:30 +0200

    Use Constants

--- solution/euler042.cpp
+++ solution/euler042.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <numeric>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
+#include <helper/Constants.hpp>
 #include <helper/FileReader.hpp>
 #include <helper/MathPackage.hpp>
 
@@ -14,11 +15,6 @@
 using namespace math;
 using namespace reader;
 
-const std::unordered_map<char, int> alphabet = { { 'A', 1 }, { 'B', 2 }, { 'C', 3 }, { 'D', 4 }, { 'E', 5 },
-        { 'F', 6 }, { 'G', 7 }, { 'H', 8 }, { 'I', 9 }, { 'J', 10 }, { 'K', 11 }, { 'L', 12 }, { 'M', 13 },
-        { 'N', 14 }, { 'O', 15 }, { 'P', 16 }, { 'Q', 17 }, { 'R', 18 }, { 'S', 19 }, { 'T', 20 },
-        { 'U', 21 }, { 'V', 22 }, { 'W', 23 }, { 'X', 24 }, { 'Y', 25 }, { 'Z', 26 } };
-
 int countTriangleWords(const std::vector<std::string>& words)
 {
     auto letterSum = [](int sum, char letter) { return sum + alphabet.find(letter)->second; };
@@ -26,8 +22,8 @@ int countTriangleWords(const std::vector<std::string>& words)
 
     for (const auto& word : words)
     {
-        if (isTriangular(std::accumulate(word.begin(), word.end(), 0, letterSum)))
-            ++triangleCnt;
+        auto wordValue = std::accumulate(word.begin(), word.end(), 0, letterSum);
+        triangleCnt += isTriangular(wordValue);
     }
 
     return triangleCnt;
