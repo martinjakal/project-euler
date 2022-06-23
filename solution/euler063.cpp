439bc1175e1a7dddb66d2c509a0a5a340390f15c
martinjakal <martinjakal93@gmail.com>
2022-06-23 23:57:18 +0200

    Typo

--- solution/euler063.cpp
+++ solution/euler063.cpp
@@ -8,7 +8,7 @@
 // The following trick is used: if a ^ b = c, then (a / 10) ^ b = c / (10 ^ b) and rhs < 1.
 // For example, 9 ^ 3 = 729 and 0.9 ^ 3 = 0.729. The result gets lower after each multiplication
 // by a / 10. When the result gets below 0.1, it means the denominator (power of 10)
-// has more digits then numerator (power of a).
+// has more digits than the numerator (power of a).
 int countNthPowerNumbers()
 {
     int nthPowerCnt = 0;
